6f20704d33e3beb09a19d4650d77e7d673878454
dminakov <dminakov@gotrg.com>
2020-10-10 12:31:43 +0300

    feat: initial commit

---
+++ src/GZipTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace GZipTest
+{
+    internal static class Program
+    {
+        private static void Main([NotNull] string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
